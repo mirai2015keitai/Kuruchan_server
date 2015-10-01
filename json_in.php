69e4b010249588a73ea51b4dea9d785f5b56e987
b1013145 <b1013145@fun.ac.jp>
2015-10-01 15:24:58 +0900

    REQUEST herasimasita

--- json_in.php
+++ json_in.php
@@ -1,14 +1,13 @@
 <?php
         if(isset($_REQUEST['st_lat']) && isset($_REQUEST['st_lng']) && isset($_REQUEST['en_lat'])
-                && isset($_REQUEST['en_lng']) && isset($_REQUEST['no_dump']) && isset($_REQUEST['low_dump']) && isset($_REQUEST['high_dump'])){
+                && isset($_REQUEST['en_lng']) && isset($_REQUEST['no_dump']) && isset($_REQUEST['high_dump'])){
                 $data1 = (String)$_REQUEST['st_lat'];
                 $data2 = (String)$_REQUEST['st_lng'];
                 $data3 = (String)$_REQUEST['en_lat'];
                 $data4 = (String)$_REQUEST['en_lng'];
                 $data5 = (String)$_REQUEST['no_dump'];
-                $data6 = (String)$_REQUEST['low_dump'];
-                $data7 = (String)$_REQUEST['high_dump'];
-                echo $data1, $data2,$data3, $data4, $data5, $data6, $data7;
+                $data6 = (String)$_REQUEST['high_dump'];
+                #echo $data1, $data2,$data3, $data4, $data5, $data6, $data7;
 
                 $db_kuru = mysql_connect('localhost', 'kurutest', 'kurutest');
                 if(!$db_kuru){
@@ -24,11 +23,11 @@
                         $enlat = (DOUBLE)$data3;
                         $enlng = (DOUBLE)$data4;
                         $n_d = (INT)$data5;
-                        $l_d = (INT)$data6;
-                        $h_d = (INT)$data7;
-                        $sql1 = sprintf("INSERT INTO LowRSC (st_lat, st_lng, en_lat, en_lng, no_dump, low_dump, high_dump)
-                                VALUES (%9.7f, %10.7f, %9.7f, %10.7f, %d, %d, %d)", $stlat, $stlng, $enlat, $enlng, $n_d, $l_d, $h_d);
+                        $h_d = (INT)$data6;
+                        $sql1 = sprintf("INSERT INTO LowRSC (st_lat, st_lng, en_lat, en_lng, no_dump, high_dump)
+                                VALUES (%9.7f, %10.7f, %9.7f, %10.7f, %d, %d)", $stlat, $stlng, $enlat, $enlng, $n_d, $h_d);
                         $result = mysql_query($sql1);
+                        echo "insert completion.";
                 }
         }else{
                 echo "error!";
